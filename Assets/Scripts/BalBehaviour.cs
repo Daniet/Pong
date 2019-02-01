736ba377457647690530b726dce35126b8dd4400
Daniel Andres <dapinto7@misena.edu.co>
2019-02-01 00:10:34 -0500

    inicio de proyecto

---
+++ Assets/Scripts/BalBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalBehaviour : MonoBehaviour {
+
+	public Transform paddle;
+	bool gamesStarted;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!gamesStarted){
+			float posDif = paddle.position.x - transform.position.x;
+			transform.position =  new Vector3(paddle.position.x - posDif, paddle.position.y, paddle.position.z);
+			if(Input.GetMouseButtonDown(0)){
+				gamesStarted = true;
+			}
+		}else{
+
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Input.mousePosition);
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = new Vector3(mousePosition.x, Mathf.Clamp(mousePosition.y, -3.8f, 3.8f), transform.position.z);
+	}
+}
